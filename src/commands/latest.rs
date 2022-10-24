144549ee7b8f4bdb8862db6368f9dc804d4211c6
Morley, Jonathan <jmorley@cvent.com>
2022-10-24 12:43:13 -0400

    test compatible with latest

--- src/commands/latest.rs
+++ src/commands/latest.rs
@@ -1,17 +1,25 @@
 use anyhow::Result;
-use asdr::core::latest::get_latest_version;
+use asdr::core::latest::{get_latest_version, get_all_latest_versions};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 pub struct LatestCommand {
-    plugin_name: String,
+    #[structopt(required_unless = "all")]
+    plugin_name: Option<String>,
     #[structopt(default_value = "[0-9]")]
     query: String,
+    #[structopt(long, conflicts_with = "plugin_name")]
+    _all: bool,
 }
 
 impl LatestCommand {
     pub fn run(&self) -> Result<()> {
-        println!("{}", get_latest_version(&self.plugin_name, &self.query)?);
+        if let Some(plugin_name) = &self.plugin_name {
+            println!("{}", get_latest_version(&plugin_name, &self.query)?);
+        } else {
+            println!("{}", get_all_latest_versions()?);
+        }
+        
         Ok(())
     }
 }
--- src/core/latest.rs
+++ src/core/latest.rs
@@ -1,7 +1,8 @@
-use crate::{call, plugin_path};
+use crate::{call, plugin_path, list_installed_plugins, list_installed_versions};
 use anyhow::{anyhow, Result};
+use itertools::Itertools;
 use regex::Regex;
-use std::process::Command;
+use std::process::{Command, self};
 
 use crate::core::list::all_plugin_versions;
 
@@ -32,6 +33,48 @@ pub fn get_latest_version(plugin_name: &str, query: &str) -> Result<String> {
             .filter(|version| !re.is_match(version))
             .map(|version| version.replace(r"^\s\+", ""))
             .last()
-            .ok_or_else(|| anyhow!("No versions found"))
+            .ok_or_else(|| anyhow!(""))
     }
+}
+
+pub fn get_all_latest_versions() -> Result<String> {
+    let installed_plugins = list_installed_plugins()?;
+
+    if installed_plugins.is_empty() {
+        return Ok(String::from("No plugins installed"));
+    }
+
+    let mut plugin_versions = Vec::new();
+
+    for plugin in list_installed_plugins()? {
+        let plugin_path = plugin_path(&plugin)?;
+        let latest_stable_path = plugin_path.join("bin").join("latest-stable");
+        
+        let version = if latest_stable_path.exists() {
+            // We can't filter by a concrete query because different plugins might
+            // have different queries.
+            call(&mut process::Command::new(&latest_stable_path)).ok()
+        } else {
+            // pattern from xxenv-latest (https://github.com/momo-lab/xxenv-latest)
+            let re = Regex::new(
+                r"(^Available versions:|-src|-dev|-latest|-stm|[-\\.]rc|-alpha|-beta|[-\\.]pre|-next|(a|b|c)[0-9]+|snapshot|master)",
+            )?;
+
+            all_plugin_versions(&plugin, None)?
+                .into_iter()
+                .filter(|version| !re.is_match(version))
+                .map(|version| version.replace(r"^\s\+", ""))
+                .last()
+        }.unwrap_or(String::from("unknown"));
+
+        let installed_status = if list_installed_versions(&plugin)?.contains(&version) {
+            "installed"
+        } else {
+            "missing"
+        };
+
+        plugin_versions.push(format!("{}\t{}\t{}", plugin, version, installed_status));
+    }
+
+    Ok(plugin_versions.into_iter().join("\n"))
 }
